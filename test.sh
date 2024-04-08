5b18b39d3ad04f6f9bfd66b0188ee3f84b51b397
ruihan11 <Abc20140120@outlook.com>
2024-04-08 10:35:59 +0800

    1

--- test.sh
+++
@@ -1,8 +0,0 @@
-cd build
-./Simulator ../test-release/add.riscv -p -s
-# ./Simulator ../test-release/mul-div.riscv 
-# ./Simulator ../test-release/n!.riscv 
-# ./Simulator ../test-release/qsort.riscv 
-# ./Simulator ../test-release/simple-function.riscv 
-
-
